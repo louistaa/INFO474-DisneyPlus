aed25af45792b9bf476ae364228202dc90ab795c
Louis <louistaa@gmail.com>
2021-12-03 01:08:06 -0800

    split strings

--- index.js
+++ index.js
@@ -26,10 +26,20 @@ processData = (dataset) => {
     dataset.forEach((element) => {
         // start at index 1 to skip show_id columns
         for (let i = 1; i < numColumns; i++) {
+
             // find corresponding map to the column
             let map = arrayOfFrequencies[i - 1];
-            
+
+            // get the row
             let value = element[columns[i]];
+
+            // does the column contain a comma?
+            if (value.includes(",")) {
+                // do not process the date, movie or description column
+                cleanRow(value, map);
+                break;
+            }
+
             // check if value is not empty, only add if it is not empty
             if (value != "") {
                 // check if key exists in the map
@@ -42,8 +52,19 @@ processData = (dataset) => {
         }
     })
 
+    console.log(arrayOfFrequencies)
+
     // arrayOfFrequencies.push(frequencyCount);
     // console.log(arrayOfFrequencies);
 
     console.log("Processing the data has ended.");
+}
+
+// helper method that takes in a row and splits the Country string
+// will increment the corresponding object to reflect frequencies
+cleanRow = (value, map) => {
+    let splittedString = value.split(", ");
+    console.log(splittedString);
+
+    // console.log(row);
 }
